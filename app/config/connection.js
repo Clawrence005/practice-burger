06cf07f967fd91c7750639d5698d23c307daf8de
colin <clawrence005@yahoo.com>
2019-04-04 21:20:31 -0500

    added .env

--- app/config/connection.js
+++ app/config/connection.js
@@ -7,14 +7,18 @@ var mysql = require("mysql");
 var keys = require("./keys");
 var secret = keys.secretPassword.secret;
 // Set up our connection information
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: secret,
-  database: "burgers_db"
-});
-
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: secret,
+    database: "burgers_db"
+  });
+}
 // Connect to the database
 connection.connect(function(err) {
   if (err) {
